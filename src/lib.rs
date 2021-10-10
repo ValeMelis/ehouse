d81897008769e295612b021deee91e7a419a17ec
Valentino Melis <tinoanime.my@gmail.com>
2021-10-10 12:56:18 +0200

    Refactoring done

--- src/lib.rs
+++ src/lib.rs
@@ -7,7 +7,17 @@ use std::fs::OpenOptions;
 use std::io::Write;
 use std::io::Read;
 
-pub fn add(args: &Vec<String>, filen: &str, dir: &str) {
+pub fn add(args: &Vec<String>) {
+    //saving the name of the file in input
+    let current_file: String = args[2].parse().unwrap();
+
+    //current directory
+    let mut dir: String = args[2].parse().unwrap();
+    dir = String::from(dir.replace("_", " ").split_whitespace().nth(0).unwrap());
+
+    //name of the file with the right extension
+    let filen: String = format!("/.ehouse/{}/{}.txt", dir, current_file);
+
     //movements of the money
     let movements: isize = args[3].parse().unwrap();
     //current date utc
@@ -58,20 +68,30 @@ pub fn add(args: &Vec<String>, filen: &str, dir: &str) {
         let mut n: isize = last_line.unwrap().to_string().parse().unwrap();
         n += 1;
 
-        file.write_all(format!("{}) {}{}\n", n, movements, date).as_bytes())
+        file.write_all(format!("{}) {} {}\n", n, movements, date).as_bytes())
             .expect("write failed");
 
         if movements>=0 {
-            println!("{}) {}{}",n,Green.paint(movements.to_string()), date);
+            println!("{}) {} {}",n,Green.paint(movements.to_string()), date);
         } else {
-            println!("{}) {}{}",n,Red.paint(movements.to_string()), date);
+            println!("{}) {} {}",n,Red.paint(movements.to_string()), date);
         }
 
         println!("Movements added");
     }
 }
 
-pub fn view(filen: &str) {
+pub fn view(args: &Vec<String>) {
+
+    //saving the name of the file in input
+    let current_file: String = args[2].parse().unwrap();
+
+    //current directory
+    let mut directory: String = args[2].parse().unwrap();
+    directory = String::from(directory.replace("_", " ").split_whitespace().nth(0).unwrap());
+
+    //name of the file with the right extension
+    let filen: String = format!("/.ehouse/{}/{}.txt", directory, current_file);
 
     //let movements: isize = args[3].parse().unwrap();
     let mut file = std::fs::File::open(filen).unwrap();
--- src/main.rs
+++ src/main.rs
@@ -12,30 +12,10 @@ fn main() {
         //assigning a variable dor the command
         let command: String = args[1].parse().unwrap();
 
-        if args.len() >= 3 {
-            //saving the name of the file in input
-            let current_file: String = args[2].parse().unwrap();
-
-            //current directory
-            let mut directory: String = args[2].parse().unwrap();
-            directory = String::from(directory.replace("_", " ").split_whitespace().nth(0).unwrap());
-
-            //name of the file with the right extension
-            let filename: String =
-                format!("/.ehouse/{}/{}.txt", directory, current_file);
-
-            if command == "view" {
-                    lib::view(&filename);
-            }
-
-            if args.len() == 4 {
-                //implementation of the add command
-                if command == "add" {
-                    lib::add(&args, &filename, &directory);
-                } else if command == "remove" {
-
-                }
-            }
+        match command.as_str() {
+            "add" => lib::add(&args),
+            "view" => lib::view(&args),
+            _ => println!("Command not valid"),
         }
     }
 }
